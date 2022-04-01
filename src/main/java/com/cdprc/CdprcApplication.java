d178d445d2be974844b56dce3e5023fb611e8c0b
Md. Reazul Haque <reazulhaque20@gmail.com>
2022-04-01 16:59:37 +0600

    1st Commit

---
+++ src/main/java/com/cdprc/CdprcApplication.java
@@ -0,0 +1,13 @@
+package com.cdprc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdprcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdprcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdprc/CdprcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdprc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdprcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
